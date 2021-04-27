f02ac95e3f71ee9c1e64c8f4d0f08ba710e307db
AlphaWangs <jw657@duke.edu>
2021-04-27 17:56:14 +0800

    A3
    A3

--- A3/HW3.R
+++ A3/HW3.R
@@ -18,36 +18,49 @@ plot(as.numeric(format(as.Date(md1$month), "%Y%m%d")),md1$crimes, xlab="Date", y
 # Exercise 2.2 Merge the two datasets by districts-units and period
 TWO<- dplyr::left_join(population, crime_long, by=c("month","district"))
 head(TWO,20)
-###########################period?
 # Exercise 2.3 Construct a panel data of unit over time with the following variables
 library(reshape)
-md2<- melt(crime_long, id=c("month", "district", "crime_type"))
-md2<- cast(md2, month+district~variable, sum)
+md2<- melt(TWO, id=c("month", "period", "district", "tot_pop", "tot_white", "tot_black", "tot_hisp",  "p50_inc", "crime_type"))
+md2<- cast(md2, month+period+district+ tot_pop+ tot_white+ tot_black +tot_hisp+  p50_inc+ crime_type~variable, sum)
+md2$crimes<-md2$crimes/md2$tot_pop
 md2<- dplyr::rename(md2, Total_crimes_per_resident=crimes)
-md3<- melt(crime_long, id=c("month", "district", "crime_type"))
+md3<- melt(TWO, id=c("month", "period", "district", "tot_pop", "tot_white", "tot_black", "tot_hisp",  "p50_inc", "crime_type"))
 md3<- md3[md3$crime_type=="violent",]
-md3<- cast(md3, month+district~variable, sum)
+md3<- cast(md3, month+period+district+ tot_pop+ tot_white+ tot_black +tot_hisp+  p50_inc+ crime_type~variable, sum)
+md3$crimes<-md3$crimes/md3$tot_pop
 md3<- dplyr::rename(md3, Violent_crimes_per_resident=crimes)
-md4<- melt(crime_long, id=c("month", "district", "crime_type"))
+md3<- md3[,-11]
+md4<- melt(TWO, id=c("month", "period", "district", "tot_pop", "tot_white", "tot_black", "tot_hisp",  "p50_inc", "crime_type"))
 md4<- md4[md4$crime_type=="property",]
-md4<- cast(md4, month+district~variable, sum)
+md4<- cast(md4, month+period+district+ tot_pop+ tot_white+ tot_black +tot_hisp+  p50_inc+ crime_type~variable, sum)
+md4$crimes<-md4$crimes/md4$tot_pop
 md4<- dplyr::rename(md4, Property_crimes_per_resident=crimes)
-md5<- dplyr::left_join(md2, md3, by=c("month","district"))
-md5<- dplyr::left_join(md5, md4, by=c("month","district"))
-md5<- dplyr::left_join(md5, population, by=c("month","district"))
-md5<- na.omit(md5[,c(-6,-7)])
-md5<- md5[,c(1:5,9,7,8,6)]
+md4<- md4[,-11]
+md5<- dplyr::left_join(md2, md3, by=c("month", "period", "district", "tot_pop", "tot_white", "tot_black", "tot_hisp",  "p50_inc"))
+md5<- dplyr::left_join(md5, md4, by=c("month", "period", "district", "tot_pop", "tot_white", "tot_black", "tot_hisp",  "p50_inc"))
 head(md5,20)
 # Exercise 3
 library(reshape)
 md6<- melt(totaldata, id=c("month", "period", "district", "tot_pop", "tot_white", "tot_black", "tot_hisp",  "p50_inc", "crime_type", "NUID", "tenure", "arrest"))
 md6<- cast(md6, month+period+district+tot_pop +tot_white+ tot_black+ tot_hisp + p50_inc +crime_type+ NUID+ tenure+ arrest~variable, sum)
-md6<- dplyr::rename(md6, Total_crimes_per_resident=crimes)
-lm_model<- lm(md6$arrest~md6$tenure+md6$Total_crimes_per_resident+md6$p50_inc+md6$tot_white+md6$tot_black+md6$tot_hisp-1)
+md6<- dplyr::rename(md6, Total_crimes=crimes)
+lm_model<- lm(md6$arrest~md6$tenure+md6$Total_crimes+md6$p50_inc+md6$tot_white+md6$tot_black+md6$tot_hisp-1)
 summary(lm_model)
 # Exercise 4
-lm_model1<- lm(md6$arrest~md6$tenure+md6$Total_crimes_per_resident+md6$p50_inc+md6$tot_white+md6$tot_black+md6$tot_hisp+md6$district+as.numeric(format(as.Date(md6$month), "%Y%m%d"))-1)
+lm_model1<- lm(md6$arrest~md6$tenure+md6$Total_crimes+md6$p50_inc+md6$tot_white+md6$tot_black+md6$tot_hisp+md6$district+factor(md6$month)-1)
 summary(lm_model1)
 # Exercise 5
-lm_model2<- lm(md6$arrest~md6$tenure+md6$Total_crimes_per_resident+md6$p50_inc+md6$tot_white+md6$tot_black+md6$tot_hisp+md6$district+as.numeric(format(as.Date(md6$month), "%Y%m%d")))
-summary(lm_model2)
+X <- as.matrix(md6[,c(-1,-2,-3,-4)])
+Y <- as.matrix(md6[,4])
+md7 <- lm(Y ~ X-1)
+summary(md7)
+X <- as.matrix(md6[,c(-1,-2,-5,-6)])
+Y <- as.matrix(md6[,2])
+md8 <- lm(Y ~ X -1)
+summary(md8)
+X <- as.matrix(md6[,c(-1,-2,-7,-8)])
+Y <- as.matrix(md6[,4])
+md9 <- lm(Y ~ X-1)
+summary(md9)
+GMM = (Y[1] - X[1,]%*%md6$coefficient[,1])[1,1]*t(X[1,])
+GMM$coefficient
